6ceb6c7385fe180dd84273767975a4288be968ba
pingf <pingf0@gmail.com>
2013-12-18 01:36:45 +0800

    initial commit, implement the basic function of zdyx bbs

---
+++ bbs.py
@@ -0,0 +1,110 @@
+#coding=utf-8
+'''Created on Dec 15, 2013 @author: Jesse Meng'''
+import os.path
+import torndb
+import tornado.httpserver
+import tornado.ioloop 
+import tornado.web 
+from tornado.options import parse_command_line
+class BBS(tornado.web.Application):
+    def __init__(self):
+        handlers = [(r"/", LoginHandler),(r"/home",HomeHandler),(r"/login",LoginHandler), (r"/logout",LogoutHandler),
+                    (r"/user/register",RegisterHandler),(r"/posts/create",PostCreateHandler),(r"/posts/(\d+)",PostDetailHandler)]
+        settings = dict(blog_title=u"ZDYX BBS PYTHON VERSION",  
+                        template_path=os.path.join(os.path.dirname(__file__), "template"),
+                        static_path=os.path.join(os.path.dirname(__file__), "static"),
+                        xsrf_cookies=True,cookie_secret="bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=",login_url="login")
+        tornado.web.Application.__init__(self, handlers, **settings)
+        self.db = torndb.Connection(host="127.0.0.1:3306", database="bbs",user="root", password="")
+
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+        return self.application.db
+    def get_current_user(self):
+        return self.get_secure_cookie("username")
+        
+class RegisterHandler(BaseHandler):
+    def get(self):
+        self.render("user/register.html", headtype="noauth")
+    def post(self):
+        username,password = self.get_argument("username"),self.get_argument("password")
+        self.db.execute("INSERT INTO users(username, password, enabled) "
+                        "VALUES (%s,%s,%s)",username,password,str(1))
+        userid = self.db.get("select id from users where username = %s",username)['id']
+        self.db.execute("INSERT INTO user_roles(userid, rolename) "
+                        "VALUES (%s, %s)",userid,"ROLE_REGULAR")
+        self.set_secure_cookie("username", username, 30)
+        self.redirect("/login")
+class LoginHandler(BaseHandler):
+    def get(self): 
+        if self.get_current_user(): 
+            self.redirect("home")
+            return
+        self.render('login.html',headtype="noauth")
+    def post(self):
+        username,password = self.get_argument("username"),self.get_argument("password")
+        results = self.db.query("select * from users") 
+        for result in results:
+            if result['username']==username and result['password']==password:
+                self.set_secure_cookie("username", username)
+                self.redirect("/home")
+                return
+        self.clear_cookie("username")
+        self.redirect("/login")
+class LogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie("username")
+        self.redirect("home")
+    
+class HomeHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        if not self.get_current_user(): 
+            self.redirect("/login")
+            return
+        posts = self.db.query("SELECT * FROM post WHERE parent_id =0 ORDER BY create_time DESC") 
+        self.render("home.html", posts=posts, headtype="auth")
+
+class PostCreateHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        self.render("posts/create.html", headtype="auth")
+    @tornado.web.authenticated
+    def post(self):
+        username=self.get_current_user()
+        title,content,parentid = self.get_argument("title"),self.get_argument("content"),self.get_argument("parentId")
+        postid = self.db.get("select id from users where username = %s",username)['id']
+        self.db.execute("INSERT INTO post(parent_id,author_name,title,content,create_time,modify_time,creator_id,modifier_id,liked_times) "
+                        "VALUES (%s, %s, %s, %s, UTC_TIMESTAMP(), UTC_TIMESTAMP(), %s, %s, %s)",parentid,username,title,content,postid,postid,str(0))
+        if parentid == '0': self.redirect("/home")
+        else: self.redirect("/posts/%s"%parentid)
+class PostDetailHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self,num):
+        posts=self.db.query("SELECT id,parent_id,author_name,title,content,create_time,modify_time,creator_id,modifier_id,liked_times FROM post "
+                            "WHERE (id = %s and parent_id = 0) OR parent_id = %s ORDER BY id asc",num,num)
+        self.render("posts/show.html",posts=posts,postid=num,posttitle=posts[0]['title'],headtype="auth")
+
+def main(): 
+    parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(BBS())
+    http_server.bind(8421)
+    http_server.start(num_processes=2)     
+    tornado.ioloop.IOLoop.instance().start()
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ db/schema.sql
@@ -0,0 +1,131 @@
+-- MySQL dump 10.13  Distrib 5.5.28, for osx10.8 (i386)
+--
+-- Host: localhost    Database: bbs
+-- ------------------------------------------------------
+-- Server version	5.5.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `post`
+--
+
+DROP DATABASE IF EXISTS `bbs`;
+
+CREATE DATABASE `bbs` character set utf8;
+
+USE `bbs`;
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(11) NOT NULL,
+  `author_name` varchar(100) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `content` text NOT NULL,
+  `create_time` datetime NOT NULL,
+  `modify_time` datetime NOT NULL,
+  `creator_id` int(11) NOT NULL,
+  `modifier_id` int(11) NOT NULL,
+  `liked_times` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_roles`
+--
+
+DROP TABLE IF EXISTS `user_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_roles` (
+  `userid` int(11) NOT NULL,
+  `rolename` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+LOCK TABLES `user_roles` WRITE;
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+INSERT INTO `user_roles` VALUES (1,'ROLE_ADMIN'),(2,'ROLE_ADMIN');
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL UNIQUE,
+  `password` varchar(20) NOT NULL,
+  `enabled` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'juntao','123456',1),(2,'huan','123456',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-10-31 20:40:05
+DROP TABLE IF EXISTS `likes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `likes` (
+  `post_id` int(11) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `likes` WRITE;
+/*!40000 ALTER TABLE `likes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `likes` ENABLE KEYS */;
+UNLOCK TABLES;
